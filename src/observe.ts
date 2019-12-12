becb1365c0ec505bd00fdcbf1142e8f186a3a502
aleics <aleics.casanovas@gmail.com>
2019-12-12 21:27:05 +0100

    Rename variables in @ObserveOn

--- src/observe.ts
+++ src/observe.ts
@@ -1,20 +1,23 @@
 import { Subject } from 'rxjs';
 
-export function ObserveOn<T>(changesProperty: string) {
+export function ObserveOn<T>(observedKey: string) {
   const listener = new Subject<T>();
 
-  return (target: any, propertyKey: string) => {
-    target[propertyKey] = listener.asObservable();
+  return (target: any, key: string) => {
+    let observedValue: T = target[observedKey];
+    target[key] = listener.asObservable();
 
-    let value = target[changesProperty];
-    const getter = () => value;
-    const setter = (val: T) => {
-      listener.next(val);
-      value = val;
-    }
+    const getter = () => observedValue;
+    const setter = (value: T) => {
+      listener.next(value);
+      observedValue = value;
+    };
 
-    Reflect.deleteProperty(target, changesProperty);
-    Reflect.defineProperty(target, changesProperty, {
+    // redefine the observed property to allow
+    // the notification of updates to the
+    // `listener`
+    Reflect.deleteProperty(target, observedKey);
+    Reflect.defineProperty(target, observedKey, {
         get: getter,
         set: setter
     });
