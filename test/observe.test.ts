c0c07a59e13bba15ccf12dfcdfed5e7af26b6510
aleics <aleics.casanovas@gmail.com>
2019-12-15 20:34:00 +0100

    Add test for multiple properties change notification

--- test/observe.test.ts
+++ test/observe.test.ts
@@ -1,9 +1,18 @@
 import { ObserveOn } from 'observer/observe';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 class SingleProperty<T> {
   foo!: T;
   fooChanges$!: Observable<T>;
+}
+
+class DoubleProperty<T> {
+  foo!: T;
+  fooChanges$!: Observable<T>;
+
+  bar!: T;
+  barChanges$!: Observable<T>;
 }
 
 test('creates observer property from any', () => {
@@ -47,5 +56,28 @@ test('notifies single change', (done) => {
   target.fooChanges$.subscribe(value => {
     expect(value).toEqual(2);
     done();
+  });
+});
+
+test('notifies multiple change', (done) => {
+  const fooObserveHandler = ObserveOn<number>('foo');
+  const barObserveHandler = ObserveOn<number>('bar');
+
+  const target = new DoubleProperty<number>();
+  fooObserveHandler(target, 'fooChanges$');
+  barObserveHandler(target, 'barChanges$');
+
+  target.foo = 2;
+  target.bar = 3;
+
+  combineLatest(target.fooChanges$, target.barChanges$).pipe(
+    first()
+  ).subscribe({
+    next: ([foo, bar]) => {
+      expect(foo).toEqual(2);
+      expect(bar).toEqual(3);
+      done();
+    },
+    complete: () => done()
   });
 });
